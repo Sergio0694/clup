4843e73f38367f697e065495d031b3bb441e7e25
Sergio Pedri <Sergio0694@live.com>
2018-05-31 03:04:53 +0200

    Added list --target-root shortcut

--- clup/Options/ListOptions.cs
+++ clup/Options/ListOptions.cs
@@ -1,4 +1,5 @@
-﻿using CommandLine;
+﻿using System;
+using CommandLine;
 
 namespace clup.Options
 {
@@ -8,7 +9,18 @@ namespace clup.Options
     [Verb("list", HelpText = "Find duplicate files and list them into a text file in the specified directory")]
     internal sealed class ListOptions : ClupOptionsBase
     {
-        [Option('t', "target", HelpText = "The target directory to use to create the list file. If not specified, the source directory will be used.", Required = false)]
+        [Option('t', "target", HelpText = "The optional target directory to use to create the list file. If not specified, the source directory will be used.", Required = false)]
         public string TargetDirectory { get; set; }
+
+        [Option("target-root", Default = false, HelpText = "Shortcut to set the target directory as the same directory used as source.", Required = false)]
+        public bool TargetRoot { get; set; }
+
+        /// <inheritdoc/>
+        public override void Validate()
+        {
+            base.Validate();
+            if (TargetRoot && !string.IsNullOrEmpty(TargetDirectory))
+                throw new ArgumentException("The --target-root and --target options can't be used at the same time");
+        }
     }
 }
